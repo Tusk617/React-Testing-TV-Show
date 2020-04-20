0348b8e086c7db47c48336d2a1b1f5f12087e441
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-20 16:20:50 -0400

    Got the fetchShow function working properly between the fetchShow.js and App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
+import {fetchShow} from "./api/fetchShow";
+
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -14,19 +16,27 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+  // useEffect(() => {
+  //   const fetchShow = () => {
+  //     axios
+  //       .get(
+  //         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  //       )
+  //       .then(res => {
+  //         setShow(res.data);
+  //         setSeasons(formatSeasons(res.data._embedded.episodes));
+  //       });
+  //   };
+  //   fetchShow();
+  // }, []);
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
-  }, []);
+    fetchShow()
+    .then(response => {
+      setShow(response);
+      setSeasons(formatSeasons(response._embedded.episodes))
+    })
+  }, [])
 
   const handleSelect = e => {
     setSelectedSeason(e.value);

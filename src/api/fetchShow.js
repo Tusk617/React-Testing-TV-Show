500d7a374a3fa24b063ce1e80aae9f24d34cd955
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-20 20:34:44 -0400

    Got my second test to pass

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -6,6 +6,6 @@ import axios from "axios"
 export const fetchShow = () => {
     return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
     .then(response => {
-        return response.data
+        return response.data;
     })
 }
